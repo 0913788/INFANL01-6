0c206d68060d01f89847aa29b86f2f3cb3b39ff4
Ricardo <0913788@hr.nl>
2016-12-17 15:36:02 +0100

    Broken added

---
+++ Nunit_Singleton_MultiThreadeded_Broken/Nunit_Singleton_MultiThreadeded_Broken/SingletonTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using NUnit.Framework;
+
+namespace Nunit_Singleton_MultiThreadeded_Broken
+{
+    [TestFixture]
+    public class SingletonTest
+    {
+        [Test]
+        public void SingletonCreationTest()
+        {
+            testCollector.newInstance();
+            Thread T1 = new Thread(() => Singleton.NewInstance(1, 0));
+            Thread T2 = new Thread(() => Singleton.NewInstance(10, 0));
+            Thread.Sleep(10);
+            T1.Start();
+            T2.Start();
+            Thread.Sleep(1000);
+            CollectionAssert.Contains(new[] { 2, 20 }, testCollector.getTotal());
+        }
+    }
+}
